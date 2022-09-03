61bcaf17cb9a777074b0eeb41d1f01091318fa97
Hilal Ahmad <hilaldev123@gmail.com>
2022-09-03 09:56:21 -0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hilal Ahmad | Web Developer</title>
+    <!-- Favicon Below -->
+    <link rel="icon" type="image/x-icon" href="/images/favicon.png">
+    <style>
+        <?php include("CSS/index.css"); ?>
+    </style>
+    <!-- Bootstrap CSS  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+   
+</head>
+<body>
+
+    <!-- Main Section  -->
+
+    <div class="custom-container hero-custom-container">
+        <div class="left-hero-section">
+            <h1 class="title-heading">Hi, i'm a <span>Web Developer</span></h1>
+            <p class="title-paragraph">As a professional web developer i will build a static as well a dynamic websites.The websites that i will create will be fully responsive especially on mobile phones.</p>
+        </div>
+        <div class="right-hero-section">
+            <img src="images/hero-image.svg" alt="main-image">
+        </div>
+    </div>
+
+    <!-- Main Section Ends Here  -->
+
+    <!-- Projects Section  -->
+
+    <h1 class="text-center mt-5 pt-3 ideas-created">Ideas Created</h1>
+
+
+    <div class="custom-container  projects-section">
+        <div class="row projects">
+            <div class="project col-sm-5 my-3 col-md-3 col-10 pb-4">
+                <img src="images/lizard.jpg" class="project-image" alt="lizard-image">
+                <h3>Lizard</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus pariatur earum doloremque fugit deserunt quibusdam.</p>
+                <div class="project-links">
+                    <a href="#">Visit</a>
+                </div>
+            </div>
+            <div class="project col-sm-5 my-3 col-md-3 col-10 pb-4">
+                <img src="images/lizard.jpg" class="project-image" alt="lizard-image">
+                <h3>Lizard</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus pariatur earum doloremque fugit deserunt quibusdam.</p>
+                <div class="project-links">
+                    <a href="#">Visit</a>
+                </div>
+            </div>
+            <div class="project col-sm-10 my-3 col-md-3 col-10 pb-4">
+                <img src="images/lizard.jpg" class="project-image" alt="lizard-image">
+                <h3>Lizard</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus pariatur earum doloremque fugit deserunt quibusdam.</p>
+                <div class="project-links">
+                    <a href="#">Visit</a>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <!-- Projects Section Ends Here  -->
+
+    <!-- About Section For 770px Above -->
+
+
+
+    <div class="custom-container about-section">
+        <div class="about-image-section">
+            <img src="images/profile-image.jpg" alt="profile-image">
+        </div>
+        <div class="about-description-section">
+            <h1 class="about-heading">Hey, I'm Hilal & <span>Coding</span> Is My Fashion</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt saepe, optio sequi a odit reiciendis vel quidem corporis qui animi?</p>
+            <div class="about-links">
+            <a href="#" >Fiverr</a>
+            <a href="#" >Upwork</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- About Section Below 770px  -->
+
+    <div class="custom-container about-card-section">
+        <img src="images/profile-image.jpg" alt="profile-img">
+        <h3>Hilal Ahmad</h3>
+        <h4>Full Stack Web Developer</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci cupiditate magnam corrupti modi eius molestiae assumenda numquam sunt exercitationem illum?</p>
+        <h3>Tech Stack</h3>
+        <div class="tech-stack">
+            <img src="images/javscript-icon.png" height="60px" widht="60px" alt="tect-stack-icons">
+            <img src="images/react-icon.png" height="60px" widht="60px" alt="tect-stack-icons">
+            <img src="images/php-icon.png" height="60px" widht="60px" alt="tect-stack-icons">
+            <img src="images/laravel-icon.png" height="60px" widht="60px" alt="tect-stack-icons">
+        </div>
+    </div>
+    
+
+    <br><br><br><br><br><br>
+    
+
+    <!-- Bootstrap Javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
